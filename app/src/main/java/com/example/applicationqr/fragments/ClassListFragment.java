dc99d8b3e6f36d318fd38dfa693765926fe79ecc
Jonathan Kong <101222148@students.swinburne.edu.my>
2020-06-12 00:06:12 +0800

    added decorator line to recyclerviews

--- app/src/main/java/com/example/applicationqr/fragments/ClassListFragment.java
+++ app/src/main/java/com/example/applicationqr/fragments/ClassListFragment.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.applicationqr.R;
 import com.example.applicationqr.adapters.ClassAdapter;
@@ -80,7 +82,10 @@ public class ClassListFragment extends Fragment
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_class_list, container, false);
         recyclerView = v.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(dividerItemDecoration);
         recyclerView.setAdapter(new ClassAdapter(request));
 
         addButton = v.findViewById(R.id.addButton);
